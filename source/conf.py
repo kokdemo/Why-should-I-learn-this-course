045893b52f4774554481e8266e058951a34dde41
kokdemo <kokdemo@gmail.com>
2014-04-16 23:42:02 +0800

    change the theme

--- source/conf.py
+++ source/conf.py
@@ -99,17 +99,17 @@ pygments_style = 'sphinx'
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = 'bootstrap'
-html_theme_path = sphinx_bootstrap_theme.get_html_theme_path()
+html_theme = 'nature'
+#html_theme_path = sphinx_bootstrap_theme.get_html_theme_path()
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
 html_theme_options = {
-    'bootswatch_theme': "flatly",
-    'navbar_class': "navbar",
-    'globaltoc_includehidden': "false",
-    'globaltoc_depth': 2,
+    # 'bootswatch_theme': "flatly",
+    # 'navbar_class': "navbar",
+    # 'globaltoc_includehidden': "false",
+    # 'globaltoc_depth': 2,
 }
 
 # Add any paths that contain custom themes here, relative to this directory.
